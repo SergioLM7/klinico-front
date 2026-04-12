e4aa2842de7d326ead73659c1bca38a30ae7a797
Sergio Lillo <sergiolillom@gmail.com>
2026-04-12 22:45:13 +0200

    Patient repository added to search patientID by surname while creating new Admission

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,9 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'core/api_client.dart';
 import 'core/theme/app_theme.dart';
 import 'data/repositories/admission_repository.dart';
-import 'data/repositories/episode_repository.dart';
 import 'data/repositories/auth_repository.dart';
+import 'data/repositories/episode_repository.dart';
+import 'data/repositories/patient_repository.dart';
 import 'data/services/auth_service.dart';
 import 'ui/viewmodels/admission_viewmodel.dart';
 import 'ui/viewmodels/episode_viewmodel.dart';
@@ -25,7 +26,8 @@ void main() {
         Provider(create: (_) => const FlutterSecureStorage()),
         Provider(
           create: (context) => ApiClient(
-            getToken: () => context.read<FlutterSecureStorage>().read(key: 'token'),
+            getToken: () =>
+                context.read<FlutterSecureStorage>().read(key: 'token'),
             // Callback (interceptor) que se invoca cuando la API devuelve 401 durante una sesión activa (token caducado en mitad del uso).
             onUnauthorized: () {
               // Capa 2: el token caducó durante la sesión → limpiar y redirigir
@@ -49,6 +51,9 @@ void main() {
         Provider(
           create: (context) => EpisodeRepository(context.read<ApiClient>()),
         ),
+        Provider(
+          create: (context) => PatientRepository(context.read<ApiClient>()),
+        ),
 
         Provider(
           create: (context) => AuthService(
@@ -61,8 +66,9 @@ void main() {
               LoginViewModel(authService: context.read<AuthService>()),
         ),
         ChangeNotifierProvider(
-          create: (context) =>
-              AdmissionViewModel(repository: context.read<AdmissionRepository>()),
+          create: (context) => AdmissionViewModel(
+            repository: context.read<AdmissionRepository>(),
+          ),
         ),
         ChangeNotifierProvider(
           create: (context) =>
